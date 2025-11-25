3719e0e12fc1f189f8a432b1e809a911ed21f4a6
kingsplace022 <kingshooter02@gmail.com>
2025-11-26 00:53:04 +0100

    Initial push to companystable/stablelinkcapitalrailway

--- stablelinkcapital/settings.py
+++ stablelinkcapital/settings.py
@@ -15,13 +15,14 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2*i#bz-_d3kayf8_^&rl+6dha(^do1m6kwa$sx0xrg02=t4(@i'
+SECRET_KEY = os.getenv("SECRET_KEY", SECRET_KEY)
 
+DEBUG = os.getenv("DEBUG", "True") == "True"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -92,18 +93,33 @@ WSGI_APPLICATION = 'stablelinkcapital.wsgi.application'
 #     }
 # }
 
+# Check if Railway DB variables are available
+USE_POSTGRES = os.getenv("DB_HOST") not in [None, "", "None"]
 
+if USE_POSTGRES:
+    print("📡 Using PostgreSQL (Railway Production Database)")
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
-        'OPTIONS': {
-            'timeout': 20,  # wait 20 seconds before failing
-        },
+    DATABASES = {
+        "default": {
+            "ENGINE": os.getenv("DB_ENGINE", "django.db.backends.postgresql"),
+            "NAME": os.getenv("DB_NAME"),
+            "USER": os.getenv("DB_USER"),
+            "PASSWORD": os.getenv("DB_PASSWORD"),
+            "HOST": os.getenv("DB_HOST"),
+            "PORT": os.getenv("DB_PORT", "5432"),
+        }
     }
-}
 
+else:
+    print("💻 Using SQLite3 (Local Development Database)")
+
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
+    
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
