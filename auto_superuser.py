2851c3e2fc585344bd2e81a4c2332c4a2b87382d
kingsplace022 <kingshooter02@gmail.com>
2025-11-26 18:05:14 +0100

    Fix auto superuser settings config

--- auto_superuser.py
+++ auto_superuser.py
@@ -1,14 +1,23 @@
-﻿from django.contrib.auth import get_user_model
-from django.db import OperationalError
+﻿import os
+import django
+
+# Tell Django where settings are
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stablelinkcapital.settings")
+django.setup()
+
+from django.contrib.auth import get_user_model
+
 User = get_user_model()
+
 username = "shapirofc"
-password = "shapirofcsite$"
-email = ""
-try:
-    if not User.objects.filter(username=username).exists():
-        User.objects.create_superuser(username, email, password)
-        print("Superuser created ✔")
-    else:
-        print("Superuser already exists ✔")
-except OperationalError:
-    print("Database not ready")
+password = "shapirofcsite1$"
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(
+        username=username,
+        password=password,
+        email=""
+    )
+    print("Superuser created successfully.")
+else:
+    print("Superuser already exists.")
