34b8a27f2e91e13bec7517eeb0b873f67993852d
kingsplace022 <kingshooter02@gmail.com>
2025-11-26 17:52:36 +0100

    Updated deployment settings and added auto superuser

---
+++ auto_superuser.py
@@ -0,0 +1,14 @@
+﻿from django.contrib.auth import get_user_model
+from django.db import OperationalError
+User = get_user_model()
+username = "shapirofc"
+password = "shapirofcsite$"
+email = ""
+try:
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username, email, password)
+        print("Superuser created ✔")
+    else:
+        print("Superuser already exists ✔")
+except OperationalError:
+    print("Database not ready")
