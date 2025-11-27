e0288b571584a671c92561a6b0a8e43a6ed5dfdd
kingsplace022 <kingshooter02@gmail.com>
2025-11-27 12:13:41 +0100

    Fix zoho callback import

--- stablelinkcapital/urls.py
+++ stablelinkcapital/urls.py
@@ -1,21 +1,19 @@
-
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from home.views import zoho_oauth_callback
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls')),  # Include the seedview app's URLs
+    path('', include('home.urls')),
     path('userprofile/', include('userprofile.urls')),
-    path('investment/', include('investment.urls')),  # Ensure 'investment.urls' is correct
+    path('investment/', include('investment.urls')),
     path('connectwallet/', include('connectwallet.urls')),
-    path('zoho/oauth/callback/', home_views.zoho_oauth_callback, name='zoho_oauth_callback'),
+
+    # Correct
+    path('zoho/oauth/callback/', zoho_oauth_callback, name='zoho_oauth_callback'),
 ]
 
-# Add this at the end to serve media files in development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
